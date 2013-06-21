fe4706ea08ded1f4b5d84721e1c9a2112b9c1664
Alexey Simbarsky <asimbarsky@gmail.com>
2013-06-21 11:34:43 +0400

    Update definitions.h

--- definitions.h
+++ definitions.h
@@ -110,12 +110,12 @@ public:
 enum LogLevel {ERROR, WARNING, INFO, DEBUG, DEBUG1};
 const std::string levelNames[5] =
 {
-    "ERROR",  //red
+    "\033[31mERROR\033[39m",  //red
     //"\033[36mWARNING\033[39m",//cyan
-    "warning",//red
-    "info",   //green
-    "debug",  //yellow
-    "debug1"  //yellow
+    "\033[31mwarning\033[39m",//red
+    "\033[32minfo\033[39m",   //green
+    "\033[33mdebug\033[39m",  //yellow
+    "\033[33mdebug1\033[39m"  //yellow
 };
 class Logger
 {
